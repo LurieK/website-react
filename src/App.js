1d625930ef35a314fe16a4356ec7945c266d5f18
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-04 16:59:20 +0800

    import components and fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/about';
+import Client from './components/client';
+import Header from './components/header';
+import Hero from './components/hero';
+import Projects from './components/projects';
+import Footer from './components/footer';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero/>
+      <About/>
+      <Client/>
+      <Projects/>
+      <Footer/>
     </div>
   );
 }
