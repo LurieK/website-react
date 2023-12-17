422e3d4c7e7529c4788980e64b712d5139b9fff7
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-17 11:50:24 +0800

    on click functions were defined backwards.
    set hover for next and back button
    
    create viewproject compoentent

--- src/components/reviewObj.js
+++ src/components/reviewObj.js
@@ -10,7 +10,7 @@ const reviews = [
     review: `"What a pleasure to work with Lurie! She developed a website for my artistic education project. On the one hand, I was very impressed with her programming skills. 
     On the other, she took the project and its external image to the next level with her creative ideas. 
     It was special to experience that level of co-creation with a developer. I really want to say: Thank you, Lurie, for your professional collaboration and support.`,
-    project: 'What Was Her Name Again"'
+    project: `What's Her Name Again?`
     
     },
     {
