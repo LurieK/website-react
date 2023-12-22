a9c05cea77f1b97adf4468885ecc286aac40a7be
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-22 12:23:35 -0500

    import tarot video add summary to projectobj
    update stack for tarot

--- src/components/projectsObj.js
+++ src/components/projectsObj.js
@@ -2,6 +2,8 @@ import todolistLaptop from '../images/todo_laptop.png';
 import todolist_mobile from '../images/todo_mobile.png'
 import todolist_video   from '../video/todos_mov.mov'
 import tarotLaptop from '../images/tarot_laptop.png';
+import tarot_mobile from '../images/tarot_mobile.png';
+import tarot_video from '../video/Tarot_video.mov';
 import WWHNA from'../images/WWHNA_computer.png';
 import Lauren from '../images/LH_computer.png';
 import pomedoro from '../images/working_phone.png';
@@ -13,18 +15,13 @@ const ProjectsObj = [
         title: `Today's Todos`,
         mainImage: todolistLaptop,
         url: '',
-        summary1: `The inspiration for this project stemed out of a curiousity about useState and if one state could be used to manage multiple events in React.
-        I asked myself what would happen if I used a count down timer, managed in state, that set off a timer, and restarted a new timer. Would that be the most efficient use of state?  If yes, would it cause errors?  If not what would be a better way to manage a timer? 
-        Already using the pomedoro method to manage my time, I opted to build a pomedoro app and explore these questions.`,
-        summary2: `In the initial stages of building the Pomodoro app, the primary focus was creating a functioning timer. This was achieved using a single state, countDownTime, to track the time.  This works alongside a useEffect hook that implements setInterval for the countdown. This setup effectively managed one timer, yet another state was needed to switch between counting down from 25 minutes or 5 minutes, which is the standard practice in a pomedoro app. This lead to creating a second state 'isWorking.' It is a boolean state that 
-        if true sets the countdown to 25 minutes and if false, 5 minutes. The synergy between isWorking and countDownTime also opened the door to add features that 
-        that make the app more user friendly as well. For instance, tracking the cumulative time spent between work and breaks, by passing isWorking as a prop to a tracker compenent, as well as integrating an alarm by passing countDownTime to a alarm component.`,
-        conclusion: `In conclusion it seems that while one state can be used to manage multiple feautres in a app, it isn't the moste effient way to make a versitiale app that tangibly useful for users.  
-        While it doesn't add additional errors, by adding one simple boolean state this app went from trakcing one timer, to becoming a daily tool that can keep a user on track, help them set goals, and manage their time more efficiently.`,
+        summary1: `This project evolved from a vanilla JavaScript CRUD site with one list to its current version– three lists in React managed in one State. Most CRUD apps I had come across only managed one list. Still, knowing one of React's benefits is reusability, I wanted to explore the power of **useState in maintaining multiple lists.** In tandem, I didn’t want to lose any of its current functionality, including the use of the **localStorage API**, and the ability to check completed items off and clear the list.`,
+        summary2: `When first tackling this project, I focused on State for each list, leaving the localStorage API for when I had a functioning app. Yet, ultimately, I found localStorage was essential for managing State. As lists are updated, they need to be added to localStorage and State, whether the user is checking them off a list or adding new items.  I could pass the data in State to localStorage, but the items would be lost on reload. Instead, I needed to set State with localStorage. As lists are updated, so is localStorage, which then passes those changes to State.`,
+        conclusion: `In summary, this project’s evolution demonstrates how the **localStorage API** can be used to manage data consistency across sessions.  When used in tandem with useState it creates persistent and reliable data management that supports a user-friendly app. `,
         key: 1,
         stack: ['React'],
         liveLink: 'https://luriek.github.io/React-todolist/',
-        github: 'https://github.com/LurieK/React-todolist',
+        github: 'https://github.com/LurieK/React-todolist/tree/LurieK-patch-4/src',
         image_mobile: todolist_mobile,
         image2: '',
         video: todolist_video,
@@ -33,15 +30,15 @@ const ProjectsObj = [
     {
         title: `Pomedoro App`,
         mainImage: pomedoro,
-        summary1: `The inspiration for this project stemed from curiosity about **useState**.  Knowing that one state could be used to manage multiple events in React I wanted to explore what would happen if I used a count down timer, managed in state, to set off a alarm, and restart a new timer. 
-        Would that be the most efficient use of state?  If yes, would it cause unpredicted errors?  If not what would be a better way to manage a timer? 
-        Already using the pomedoro method to manage my time at work, I opted to build a pomedoro app and explore these questions.`,
-        summary2: `In the initial stages of building the app, the primary focus was creating a functioning timer. This was achieved using a single state, **countDownTime**, to track the time.  This worked alongside a **useEffect** hook that implemented **setInterval** for the countdown rednered on the screen. This setup effectively managed one timer, and set an alarm off, 
-        yet another state was needed to switch between counting down from 25 minutes or 5 minutes, which is the standard practice in a pomedoro app. 
-        This lead to creating a second state **isWorking**, a boolean state that if true sets the countdown to 25 minutes and if false, 5 minutes. The synergy between isWorking and countDownTime also opened the door to add features that 
-        that make the app more user friendly as well. For instance, tracking the cumulative time spent between work and breaks, by passing isWorking as a prop to a tracker compenent, as well as integrating pause and restart buttons that let users control the timer.`,
-        conclusion: `In conclusion it seems that while one state can be used to manage multiple feautres in a app, it isn't the moste effient way to make a versitiale app that tangibly useful for users.  
-        While it doesn't add additional errors, by adding one simple boolean state this app went from trakcing one timer, to becoming a daily tool that can keep a user on track, help them set goals, and manage their time more efficiently.`,
+        url:'',
+        summary1: `The inspiration for this project stemmed from curiosity about **useState**.  Knowing that one State could manage multiple events in React, I wanted to explore what would happen if I used one State to handle a countdown timer. This single State would also set off an alarm and restart a new timer. 
+        Would that be a possible use of State?  If yes, would it be efficient or cause unpredicted errors?  If not, what would be a better way to manage a timer? 
+        Already using the Pomodoro method to manage my time at work, I opted to build a Pomodoro app and explore these questions.`,
+        summary2: `In the initial stages of building the app, the primary focus was creating a functioning timer. This was achieved using a single State, **countDownTime**, to track the time.  It worked alongside a **useEffect** hook that implemented **setInterval** for the countdown rendered on the DOM. This setup effectively managed one-timer and set the alarm off. Yet another State was needed to switch between counting down from 25 minutes to 5 minutes, which is the standard practice in a Pomodoro app. 
+        This lead to creating a second State **isWorking**, a boolean State that, if true, sets the countdown to 25 minutes and, if false, 5 minutes. The synergy between isWorking and countDownTime also opened the door to adding features that 
+        make the app more user-friendly. For instance, tracking the cumulative time spent between work and breaks by passing isWorking as a prop to a tracker component, as well as integrating pause and restart buttons that let users control the timer.`,
+        conclusion: `In conclusion, it seems that while one State can be used to manage multiple features in an app, it isn't the most efficient way to make a versatile app that tangibly useful for users.  
+        While it doesn't add additional errors, by adding one simple boolean State, this app went from tracking one timer, to becoming a daily tool that can keep a user on track, help them set goals, and manage their time more efficiently.`,
         key: 5,
         stack: ['React'],
         liveLink: '',
@@ -55,17 +52,18 @@ const ProjectsObj = [
      {
         title: `Daily Tarot`,
         mainImage: tarotLaptop,
-        summary1: `The inspiration for this project stemed out of a curiousity about useState and if one state could be used to manage multiple events in React.
-        I asked myself what would happen if I used a count down timer, managed in state, that set off a timer, and restarted a new timer. Would that be the most efficient use of state?  If yes, would it cause errors?  If not what would be a better way to manage a timer? 
-        Already using the pomedoro method to manage my time, I opted to build a pomedoro app and explore these questions.`,
-        summary2: `In the initial stages of building the Pomodoro app, the primary focus was creating a functioning timer. This was achieved using a single state, countDownTime, to track the time.  This works alongside a useEffect hook that implements setInterval for the countdown. This setup effectively managed one timer, yet another state was needed to switch between counting down from 25 minutes or 5 minutes, which is the standard practice in a pomedoro app. This lead to creating a second state 'isWorking.' It is a boolean state that 
-        if true sets the countdown to 25 minutes and if false, 5 minutes. The synergy between isWorking and countDownTime also opened the door to add features that 
-        that make the app more user friendly as well. For instance, tracking the cumulative time spent between work and breaks, by passing isWorking as a prop to a tracker compenent, as well as integrating an alarm by passing countDownTime to a alarm component.`,
-        conclusion: `In conclusion it seems that while one state can be used to manage multiple feautres in a app, it isn't the moste effient way to make a versitiale app that tangibly useful for users.  
-        While it doesn't add additional errors, by adding one simple boolean state this app went from trakcing one timer, to becoming a daily tool that can keep a user on track, help them set goals, and manage their time more efficiently.`,
+        summary1: `Initially, this project had all its operations bundled together, including the API function and the JavaScript logic, in one file. Recognizing the need for a more secure approach, I wanted to explore **Express.js** to understand how it could be utilized to enhance website security. This led me to a deeper understanding of how Express as a modular framework not only secures sensitive data but helps keep code clean and running efficiently.`,
+
+        summary2: `The first step in this evolution was moving the API key to a separate .env file and establishing a Node.js server using Express.js. This setup allowed me to create internal endpoints for **handling asynchronous requests securely and efficiently**. By keeping client-side DOM manipulation and API request handling separate, the project aso gained clarity and efficiency. While I hadn’t considered this in the beginning making the project modulate also enabled me to move the cards data into its own file, further modularizing the project.`,
+
+        conclusion: `My exploration concluded that Express.js not only bolsters API security but also enhances code efficiency and scalability. This transition resulted in cleaner, more organized code, laying a strong foundation for future feature additions and ensuring smooth scalability without unexpected errors. As the project continues to evolve, this structure will be instrumental in its growth and stability.`,
         url: '',
         key: 2,
-        stack: ['TypeScript', 'Async API']
+        stack: ['JavaScript','Express JS','Async API'],
+        liveLink:'https://working-state--famous-pithivier-c2c9b0.netlify.app/',
+        image_mobile: tarot_mobile,
+        image2: '',
+        video: tarot_video
 
     },
     {
