bfac8cb83bc8e7edf50977e9cd441490c411a050
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2024-02-08 15:01:03 +0800

    update project page
    add Laurens review to review page

--- src/components/reviewObj.js
+++ src/components/reviewObj.js
@@ -14,21 +14,14 @@ const reviews = [
     
     },
     {
-    name: 'Jasmin Mittag',
+    name: 'Lauren Harkness',
     image: holder,
-    review: `"What a pleasure to work with Lurie! She programmed the website for my artistic education project. On the one hand, I was very impressed with her programming skills. 
-    On the other, she took the project and its external image to the next level with her creative ideas. 
-    It was special to experience that level of cocreation with a programmer. I really want to say: thank you, Lurie, for your professional cooperation and support.`,
-    project: 'What Was Her Name Again"'
-    
-    },
-    {
-    name: 'Jasmin Mittag',
-    image: holder2,
-    review: `"What a pleasure to work with Lurie! She programmed the website for my artistic education project. On the one hand, I was very impressed with her programming skills. 
-    On the other, she took the project and its external image to the next level with her creative ideas. 
-    It was special to experience that level of cocreation with a programmer. I really want to say: thank you, Lurie, for your professional cooperation and support.`,
-    project: 'What Was Her Name Again"'
+    review: `My website needs have evolved over the years and today it seems like there are layers upon 
+    layers of old and new platforms and plugins, making it challenging to manage my site. Lurie came in 
+    and dug deep into my Shopify setup to help untangle a few legacy issues. She accomplished our goals
+    efficiently, and without causing any errors to existing functionality. It's a pleasure to work with a 
+    woman who is both professional and friendly as well!`,
+    project: 'Shopify Website'
     
     }
     
